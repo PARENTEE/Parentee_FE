72e61f8bf5a44ac82dde647363957f1e324bd129
Lamiee <lambph1204@gmail.com>
2025-09-29 21:20:00 +0700

    restyle all font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:parentee_fe/app/theme/app_colors.dart';
 import 'package:parentee_fe/features/auth/screens/HomePage/home_page.dart';
 import 'package:parentee_fe/features/auth/screens/Onboarding/onboarding-page.dart';
@@ -14,13 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Onboarding Demo',
+      title: 'Parentee',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: AppColors.primary_button,
         ).copyWith(background: Colors.white, surface: Colors.white),
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
+        // textTheme: GoogleFonts.robotoTextTheme(),
+        textTheme: GoogleFonts.nunitoSansTextTheme(),
       ),
       // Trang khởi động là OnboardingPage
       home: const OnboardingPage(),
